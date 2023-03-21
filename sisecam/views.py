2c6152cd1e896ec06ba36f6f7bb85ce54bd44eb7
farad213 <farad213@gmail.com>
2023-03-21 18:40:35 +0100

    login required and group check to access sisecam

--- sisecam/views.py
+++ sisecam/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import Piles
 from django.db.models import Q
+from django.contrib.auth.decorators import user_passes_test, login_required
 
+def Şişecam_group_check(user):
+    return user.groups.filter(name='Şişecam').exists()
+
+
+@login_required
+@user_passes_test(Şişecam_group_check)
 def buildings(request):
     context = {}
     return render(request, "sisecam/buildings.html", context)
 
+
+@login_required
+@user_passes_test(Şişecam_group_check)
 def ajax_buildings_select(request):
     partial_sorszam = str(request.GET["sorszam"])
     options = Piles.objects.filter(Q(sorszam__icontains=partial_sorszam)).order_by("sorszam")
@@ -13,6 +23,9 @@ def ajax_buildings_select(request):
     context = {"options": options}
     return render(request, "sisecam/ajax/buildings_select.html", context)
 
+
+@login_required
+@user_passes_test(Şişecam_group_check)
 def ajax_buildings_calculate(request):
     sorszam = str(request.GET["sorszam"])
     select = request.GET["select"]
