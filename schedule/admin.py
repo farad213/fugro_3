c6820b763d732c984bac8bd99c5cf43ace494127
farad213 <farad213@gmail.com>
2023-03-28 19:49:02 +0200

    search function on django admin page for dates, artifacts and profiles

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from .models import Vehicle, Project, Date, DateBoundWithProject, Profile, Artifact, Subproject, SIT_project, SIT_with_date
 
+class DateAdmin(admin.ModelAdmin):
+    search_fields = ['date', "is_holiday"]
+    change_list_template = "admin/date_change_list.html"
+
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ['profile']
+
+class ArtifactAdmin(admin.ModelAdmin):
+    search_fields = ['artifact']
 
 admin.site.register(Vehicle)
 admin.site.register(Project)
-admin.site.register(Date)
+admin.site.register(Date, DateAdmin)
 admin.site.register(DateBoundWithProject)
 
-admin.site.register(Profile)
-admin.site.register(Artifact)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Artifact, ArtifactAdmin)
 admin.site.register(Subproject)
 
 admin.site.register(SIT_project)
